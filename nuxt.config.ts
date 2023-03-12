e4339d41683718cf6114ae3a01775983e49a43a1
simon <29374939+prclm@users.noreply.github.com>
2023-03-12 22:59:44 +0100

    dev: proxy setup for live testing

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
-})
+  routeRules: {
+    "/*": { cors: true },
+  },
+  devServer: {
+    port: 5555,
+  },
+  vite: {
+    server: {
+      strictPort: true,
+      hmr: {
+        protocol: "wss",
+        clientPort: 443,
+        port: 5551,
+        path: "/socket.io",
+      },
+    },
+  },
+});
