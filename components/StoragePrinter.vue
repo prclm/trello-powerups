42c2cd99c437d315b400a25babbb759993891ee4
simon <29374939+prclm@users.noreply.github.com>
2023-03-19 20:56:19 +0100

    StoragePrinter: added total storage size

--- components/StoragePrinter.vue
+++ components/StoragePrinter.vue
@@ -1,9 +1,10 @@
 <template>
-  <pre>{{ storageWithSize }}</pre>
+  <pre>PowerUpStorage{{ "&nbsp;" }}({{ storageSizeRef }}):<!--
+  -->{{ storageWithSize }}</pre>
 </template>
 
 <script lang="ts" setup>
-import { computed, inject } from "#imports";
+import { ref, computed, inject } from "#imports";
 import { useTrello, TRELLO_CTX_SYMBOL } from "~~/composables/trello";
 
 /** inject and use trello context */
@@ -11,8 +12,10 @@ const trelloCtx = inject(TRELLO_CTX_SYMBOL) as ReturnType<typeof useTrello>;
 const { getStoredDataRef } = trelloCtx;
 
 const storage = getStoredDataRef();
+const storageSizeRef = ref();
 const storageWithSize = computed(() => {
   if (typeof storage.value !== "object") return "stroage not yet loaded...";
+  let storageSize = 0;
 
   const storageWithSizeObject: any = {};
   Object.entries(storage.value).forEach((scopeItem) => {
@@ -27,11 +30,15 @@ const storageWithSize = computed(() => {
       const entrySize =
         visibilityKey.length + JSON.stringify(visibilityValue).length;
 
+      storageSize += entrySize;
+
       scopeValueWithSize[`${visibilityKey} (${entrySize}/4096)`] =
         visibilityValue;
     });
     storageWithSizeObject[scopeKey] = scopeValueWithSize;
   });
+
+  storageSizeRef.value = storageSize;
   return storageWithSizeObject;
 });
 </script>
